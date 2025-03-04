a483931ffc37a397bfed1bf3bdfc8de3b65df36b
Natalia Ivakina <nata.iva.w@gmail.com>
2025-03-03 21:58:17 -0400

    style fixed, button moved at top of card

--- script.js
+++ script.js
@@ -7,68 +7,40 @@ document.querySelectorAll('.project-card').forEach(card => {
     });
 });
 
+const button = document.querySelector('#toggleButton');
+const image1 = document.querySelector('#image1');
+const image2 = document.querySelector('#image2');
+const skills = document.querySelector('#skills');
+const facts = document.querySelector('#facts');
+
 /**
- * change img if mouse is moving
+ * switcher info and photo
  */
-// document.querySelector('.photo').addEventListener('mousemove', function(e) {
-//     const rect = this.getBoundingClientRect();
-//     const x = (e.clientX - rect.left) / rect.width * 100;
-//     const y = (e.clientY - rect.top) / rect.height * 100;
-//
-//     this.querySelector('#image1').style.clipPath = `inset(0 ${100 - x}% 0 0)`;
-// });
-//
-// document.querySelector('.photo').addEventListener('mouseleave', function() {
-//     this.querySelector('#image1').style.clipPath = `inset(0 0 0 0)`;
-// });
+button.addEventListener('click', () => {
+    button.classList.toggle('active');
 
-// document.querySelector('#toggleButton').addEventListener('click', function(e) {
-//     // Меняем текст кнопки
-//     const text = document.querySelector('#toggleButton');
-//     if (text.textContent === 'Switch to Facts') {
-//         text.textContent = 'Switch to Skills';
-//     } else {
-//         text.textContent = 'Switch to Facts';
-//     }
-//
-//     const rect = this.getBoundingClientRect();
-//     const x = (e.clientX - rect.left) / rect.width * 100;
-//     const y = (e.clientY - rect.top) / rect.height * 100;
-//
-//     this.querySelector('#image1').style.clipPath = `inset(0 ${100 - x}% 0 0)`;
-// });
-
-document.querySelector('#toggleButton').addEventListener('click', function() {
-    const image1 = document.querySelector('#image1');
-    const image2 = document.querySelector('#image2');
-    const skills = document.querySelector('#skills');
-    const facts = document.querySelector('#facts');
-    const text = document.querySelector('#toggleButton');
-
-    if (text.textContent === 'Switch to Facts') {
-        text.textContent = 'Switch to Skills';
-
-        //hide
+    if (button.classList.contains('active')) {
+        //Skills
         image2.style.opacity = '0';
         image2.style.clipPath = 'inset(0 0 0 100%)';
-        //show
         image1.style.opacity = '1';
         image1.style.clipPath = 'inset(0 0 0 0)';
 
         skills.style.visibility = 'hidden';
+        skills.style.opacity = '0';
         facts.style.visibility = 'visible';
+        facts.style.opacity = '1';
     } else {
-        text.textContent = 'Switch to Facts';
-
-        //hide
+        //Facts
         image1.style.opacity = '0';
         image1.style.clipPath = 'inset(0 100% 0 0)';
-        //show
         image2.style.opacity = '1';
         image2.style.clipPath = 'inset(0 0 0 0)';
 
-        facts.style.visibility = 'hidden';
         skills.style.visibility = 'visible';
+        skills.style.opacity = '1';
+        facts.style.visibility = 'hidden';
+        facts.style.opacity = '0';
     }
 });
 
