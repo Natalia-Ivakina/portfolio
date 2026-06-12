b746e99676dcb634b3e466fb78778c208116d064
Natalia Ivakina <nata.iva.w@gmail.com>
2026-06-12 00:39:11 -0300

    Info layout design

--- js/switch_photo.js
+++ js/switch_photo.js
@@ -1,36 +1,4 @@
-const button = document.querySelector("#toggleButton");
-const image1 = document.querySelector("#image1");
-const image2 = document.querySelector("#image2");
-const skills = document.querySelector("#skills");
-const facts = document.querySelector("#facts");
-
-/**
- * switcher info and photo
- */
-button.addEventListener("click", () => {
-  button.classList.toggle("active");
-
-  if (button.classList.contains("active")) {
-    //Skills
-    image2.style.opacity = "0";
-    image2.style.clipPath = "inset(0 0 0 100%)";
-    image1.style.opacity = "1";
-    image1.style.clipPath = "inset(0 0 0 0)";
-
-    skills.style.visibility = "hidden";
-    skills.style.opacity = "0";
-    facts.style.visibility = "visible";
-    facts.style.opacity = "1";
-  } else {
-    //Facts
-    image1.style.opacity = "0";
-    image1.style.clipPath = "inset(0 100% 0 0)";
-    image2.style.opacity = "1";
-    image2.style.clipPath = "inset(0 0 0 0)";
-
-    skills.style.visibility = "visible";
-    skills.style.opacity = "1";
-    facts.style.visibility = "hidden";
-    facts.style.opacity = "0";
-  }
+const root = document.getElementById("main-content");
+document.getElementById("toggleButton").addEventListener("click", () => {
+  root.dataset.state = root.dataset.state === "skills" ? "facts" : "skills";
 });
